b604dfd893f19c197ae498dce9176f0c334c1dcb
shirasvm <shiras.astabits@gmail.com>
2023-05-11 11:54:55 +0530

    contact server v1

---
+++ index.js
@@ -0,0 +1,17 @@
+const jsonServer=require('json-server')
+
+const server=jsonServer.create()
+
+const router=jsonServer.router("db.json")
+
+const middleware=jsonServer.defaults()
+
+const port = process.env.port || 3000
+
+server.use(middleware)
+server.use(router)
+
+server.listen(port, ()=>{
+    console.log(`contact server app started at ${port}`);
+})
+
